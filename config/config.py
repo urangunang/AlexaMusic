b0b15de6010914ccc640ee3be465f89735a99453
urangunang <145663184+urangunang@users.noreply.github.com>
2023-10-13 08:01:42 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -13,7 +13,7 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", ""))
+API_ID = int(getenv("API_ID", "24465982"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
